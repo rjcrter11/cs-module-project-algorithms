d46a598a093d6faefe3496988067619622a756af
Rachel <rjcrter11@gmail.com>
2020-06-18 17:42:08 -0500

    completes more effective solutions for all files

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,29 +4,39 @@ Returns: an integer
 '''
 # Assumptions:
 # Good candidate for recursion
-#
 
 
-def eating_cookies(n):
+def eating_cookies(n, cache=None):
     # Use recursion a la fibonacci
-    # Set base cases w/ 3 down
+    # Set base cases
+    # Set up cache for larg inputs
 
     # base cases:
+    if n < 0:
+        # negative numbers means we didnt find one of the ways
+        return 0
     if n == 0:
+        # getting to 0 means we found a way to eat our cookies
         return 1
-    elif n == 1:
-        return 1
-    elif n == 2:
-        return 2
-    elif n == 3:
-        return 4
-    # Recursion w/ 3 values
-    return eating_cookies(n - 1) + eating_cookies(n-2) + eating_cookies(n-3)
+
+    # if cache is None
+        # create the cache
+    if cache is None:
+        cache = {}
+    # check if cache exists
+    # check if n is in cache
+        # return cache[n]
+    if cache and cache[n]:
+        return cache[n]
+
+    cache[n] = eating_cookies(
+        n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    return cache[n]
 
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 6
+    num_cookies = 5
 
     print(
         f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,32 +2,47 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+from collections import deque
 
+
+# def sliding_window_max(nums, k):
+
+#     n = len(nums)
+#     # store max value in result array
+#     result = []
+#     # Nested Loop
+#     # Outer loop to go through start index to kth
+#     for i in range(n - k+1):
+#         max_val = nums[i]
+#         # Inner loop for k iterations
+#         for j in range(i, i + k):
+#             # check for values larger than max_val
+#             if max_val < nums[j]:
+#                 max_val = nums[j]
+#         # Append those values to result array
+#         result.append(max_val)
+#     # return result array
+#     return result
 
 def sliding_window_max(nums, k):
-
     n = len(nums)
-    # store max value in result array
-    result = []
-    # Nested Loop
-    # Outer loop to go through start index to kth
-    for i in range(n - k+1):
-        max_val = nums[i]
-        # Inner loop for k iterations
-        for j in range(i, i + k):
-            # check for values larger than max_val
-            if max_val < nums[j]:
-                max_val = nums[j]
-        # Append those values to result array
-        result.append(max_val)
-    # return result array
+    result = [0] * (n - k + 1)
+    queue = []
+    for i in range(n):
+        if queue and queue[0] == i - k:
+            del queue[0]
+        while queue and nums[queue[-1]] < nums[i]:
+            del queue[-1]
+        queue.append(i)
+        if i + 1 >= k:
+            result[i - k + 1] = nums[queue[0]]
     return result
 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
-    # arr = [1, 3, -1, -3, 5, 3, 6, 7]
-    # k = 3
+    arr = [1, 3, -1, -3, 5, 3, 6, 7]
+    k = 3
     '''
     Window position                Max
     ---------------               -----
@@ -40,6 +55,5 @@ if __name__ == '__main__':
 
     '''
 
-
-# print(
-#     f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+    print(
+        f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
